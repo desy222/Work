3aace98272c8f7fd2a84438c9a1ac9c59a9099ab
Desislava Stoyanova <stoyanova.d.h@gmail.com>
2017-02-05 23:52:15 +0200

    Update increasing-sequence.js

--- increasing-sequence.js
+++ increasing-sequence.js
@@ -1,33 +1,23 @@
+/**Write a program that finds the length of the maximal increasing sequence in an array of N integers. */
+/**OUTPUT : Print the length of the maximal increasing sequence */
+
 function findMaxIncrSeq(arr) {
 
     var currentSequence = 1,
         longestSequence = 1,
         indexOfLongest;
 
-    for (var i = 0; i < arr.length; i++) {
+    for (var i = 0; i < arr.length; i++) { /**Looping through the array from 0 index */
         if (arr[i] < arr[i + 1]) {
-            currentSequence++;
-        } else {
-            if (longestSequence < currentSequence) {
+            currentSequence++; /** adding 1 to the counter if the second index is bigger than the previous*/
+        } else if (longestSequence < currentSequence) {
                 longestSequence = currentSequence;
-                console.log(currentSequence);
                 indexOfLongest = i - currentSequence + 1;
-                //console.log(i);
-            }
-            currentSequence = 1;
+            
+            currentSequence = 1; //if two elements are not equal, temporary counter = 1
         }
     }
-
-    // if (longestSequence < 2) {
-       
-    // }
-    // else {
-    //     var result = arr.slice(indexOfLongest, indexOfLongest + longestSequence);
-        
-    // }
-
+     console.log(longestSequence);
 }
 
-//findMaxIncrSeq([3, 2, 3, 4, 2, 2, 4]);
-findMaxIncrSeq([3, 5, 4, 6, 1, 2, 3, 6, 10, 32]);
-//findMaxIncrSeq([3, 2, 1]);
+findMaxIncrSeq([3, 2, 3, 4, 5,6, 2, 2, 4]); /** 5 */
