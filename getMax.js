5cbee87c0c4469447cf87f008319e0099e10965f
Desislava Stoyanova <stoyanova.d.h@gmail.com>
2017-02-06 11:56:55 +0200

    Update getMax.js

--- getMax.js
+++ getMax.js
@@ -1,7 +1,9 @@
+/**Write a method GetMax() with two parameters that returns the larger of two integers. */
+
 function numbers(args) {
     args.sort(function(a,b){
-        return b-a;
+        return b-a; /** compare a and b and the sort function takes care to find the biggest one */
     });
     console.log('The biggest number is' + args[0]);
 }
-numbers([4, 2, 5, 1, 333]);
+numbers([4, 2, 5, 1, 333]);  /**The biggest number is 333 */
