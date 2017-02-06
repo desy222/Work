e5034bb7ecbe8c42bd3c4f13cf329bd15105e099
Desislava Stoyanova <stoyanova.d.h@gmail.com>
2017-02-06 15:06:05 +0200

    Update prime.js

--- prime.js
+++ prime.js
@@ -1,22 +1,32 @@
-
+/**Write a program that finds all prime numbers in the range [1 ... N]. 
+ */
 function printPrime(value) {
-    var primes = []; /// празен масив
-    for(var i = 2; i < value; i++) {
-        primes[i] = true; // вс стойности за i са предефинирани с true;
+    var primes = []; /// empty array
+    for(var i = 2; i < value; i++) { 
+        primes[i] = true; // every index is predefined with 'true';
     }
     var limit = Math.sqrt(value); 
+    /** Here we make two for loops where the decision is taken . 
+        If current index answer these conditions , the number is prime or not!*/
     for(var i = 2; i < limit; i++) { 
         if(primes[i] === true) {
             for(var j = i * i; j < value; j += i) {
-                
-                primes[j] = false;
+                primes[j] = false; 
             }
         }
     }
-    for(var i = 2; i < value; i++) {
+    for(var i = 2; i < value; i++) { 
         if(primes[i] === true) {
             console.log(i + " " + primes[i]);
         }
     }
 }
-printPrime(12);
+printPrime(12); 
+/**printprime 
+Logs : 
+2 true
+3 true
+5 true
+7 true
+11 true
+*/
