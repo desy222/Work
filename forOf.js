b4a615f213bfb5673afa79c8bd329fe0c544a10b
Desislava Stoyanova <stoyanova.d.h@gmail.com>
2017-02-06 17:08:00 +0200

    Add files via upload

---
+++ forOf.js
@@ -0,0 +1,15 @@
+ /**program which gves you how many times a word is used in array 
+  * 
+ */
+ const wordArray = 'word number word pesho word'
+    .split(' ');
+    let words = {};
+    for(let word of wordArray) {
+    //     words[word]= true; //or
+    if(words.hasOwnProperty(word)){ /**Determines whether an object has a property with the specified name */
+        words[word]  +=1 ;
+    } else {
+        words[word] = 1;
+    }}
+    
+    console.log(words);
