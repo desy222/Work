28d282cdf15d0ce3b9a2bbdc215dfe6ad3e07549
Desislava Stoyanova <stoyanova.d.h@gmail.com>
2017-02-06 11:38:43 +0200

    Update findmaxSequence.js

--- findmaxSequence.js
+++ findmaxSequence.js
@@ -1,29 +1,24 @@
+/**Write a program that finds the length of the maximal sequence of equal elements in an array of N integers. */
+/**Print the length of the maximal sequence */
+
 function findMaxSeq(nums) {
     var currentSequence = 1,
         longestSequence = 1,
-        maxSeqNum = nums[0]; 
+        maxSeqNum = nums[0]; /** */
 
     for (var i = 1; i < nums.length; i++) {
-        if(nums[i-1] === nums[i]) {
+        if(nums[i-1] === nums[i]) { /**start from the last index and compare with the first */
             currentSequence++;
             if (currentSequence >= longestSequence) {
                 longestSequence = currentSequence; 
-                maxSeqNum = nums[i]; 
-                console.log(currentSequence);
+                maxSeqNum = nums[i];  /**max sequence number is = to current index value */
+           //   console.log(nums[i]);
             }
         }
         else {
-            currentSequence = 1; //if two elements are not equals, temporary counter = 1
+            currentSequence = 1; //if two elements are not equal, temporary counter = 1
         }
-    }
-
-    // create arr with the results
-    var result = [];
-    for (var i = 0; i < longestSequence; i++) {
-        result.push(maxSeqNum);
-    }
-    console.log(maxSeqNum)
-
+    } console.log(maxSeqNum);
 }
 
-findMaxSeq([2, 1, 1, 2, 3, 3, 2, 2, 2, 1]);
+findMaxSeq([2, 1, 1, 2, 3, 3, 3, 3, 2, 2, 1]); // OUTPUT : 3
