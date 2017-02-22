84f77577f95ca9fd62e5962904bc00c066e8165c
Desislava Stoyanova <stoyanova.d.h@gmail.com>
2017-02-22 11:49:15 +0200

    Update task.js

--- task.js
+++ task.js
@@ -1,37 +1,47 @@
 /**Write a method that returns the index of an element in array that is largest than its neighbours compared 
 than the rest of the numbers */
+function solve(arr) {
+    var arr = [];
 
-function solve(arr){
-    let largest = 0;
-    var newObj = {}
-    
-    for(let i=1; i< arr.length; i+=1) {
-        if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
-
-            largest = arr[i];         
-            newObj.arrLargest = largest;  /**
-             assign largest to the newObj arrLargest property */
-            
-            function findDistance () { /**function which compares the distances between neighbours of largest el-ts */
-                
-                if(largest - arr[i-1] > largest - arr[i + 1]) {
-                    distance = largest - arr[i-1]; 
-                } 
-                else {
-                    distance = largest - arr[i+1];
-                }
-
-                 newObj.arrDistance = distance; /**assign the distance to the arrDistance propery of newObj */
-                 console.log(newObj) 
-                 /**Here the program logs : 
-                 { arrLargest: -1, arrDistance: 49 }
-                 { arrLargest: -2, arrDistance: 298 } */
-            }
-
-            findDistance();
-         }
+    while(arr.length < 35){             /**array generator with 35 elements */
+        var randomnumber = Math.ceil(Math.random()*400)
+        if(arr.indexOf(randomnumber) > -1) continue;
+        arr[arr.length] = randomnumber;
     }
+
+    let currentLargestNum = 0;    
+    var newArr = [];    /**create array where we will put the new objects */
+    let distance = 0; /**sum of the distancies between the currentlargest number and its neighbours */
+    var myResult ;
+
+        for(let i=1; i< arr.length; i+=1) {
+            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+                currentLargestNum = arr[i];  
+                distance = (currentLargestNum - arr[i-1]) + (currentLargestNum - arr[i+1]); 
+
+                var result = new Object;
+                result.dist = distance; /**result.dist and result.num are the new prop of object result */
+                result.num = currentLargestNum;
+                newArr.push(result); 
+                
+            }  
+                     
+        }
+
+        myResult = newArr[0]; /**Here we say that myResult is taking the value of newArr index  */
+        for (var i = 0; i < newArr.length; i++) {   /**Starts a loop which checks for the largst distance in every object that we have in newArr */
+            if (newArr[i].dist > myResult.dist) {
+                myResult = newArr[i];   /**Initialize the maximum distance with the object that contains it */
+                }
+        }
+        console.log(`This is the array that contains all the objects that we compare
+        `,newArr); 
+
+        console.log(`This is the object that contains number bigger than its neighbours
+        and compared to the other largest nums it has the biggest difference 
+        with its neighbours 
+        `, myResult);
+         
+
 };
-
-solve([-30 ,-1, -50, -80, -2 ,-300]); 
-
+solve(); 
